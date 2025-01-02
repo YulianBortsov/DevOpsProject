2555b2be2752467b296953748b0c25b7511b3a79
Yulian <bortsovyulian@gmail.com>
2025-01-02 14:55:16 +0200

    test cicd backend

--- app-repo/code/app.py
+++ app-repo/code/app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 CORS(app)
 
 # test the CI/CD pipeline
-test = "testtt"
+test = "test"
 
 # Database configuration from environment variables
 DB_HOST = os.getenv('DATABASE_HOST', 'web-app-application-postgresql')
