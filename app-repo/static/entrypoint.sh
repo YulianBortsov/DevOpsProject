d25284c144b8b55bc4c1e5ff0dbb249b22f349d1
Yulian <bortsovyulian@gmail.com>
2025-01-02 14:52:35 +0200

    fixed subsenv

--- app-repo/static/entrypoint.sh
+++ app-repo/static/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-envsubst < /usr/share/nginx/html/script.js.template > /usr/share/nginx/html/script.js
+envsubst '${API_URL}' < /usr/share/nginx/html/script.js.template > /usr/share/nginx/html/script.js
 nginx -g 'daemon off;'
