088b1e6fd84782d01fa053774b4ff466631a7a0d
Yulian <bortsovyulian@gmail.com>
2025-01-08 13:02:07 +0200

    updated test var in app.py

--- app-repo/code/app.py
+++ app-repo/code/app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 CORS(app)
 
 # test the CI/CD pipeline
-test = "test123"
+test = "test123456"
 
 # Database configuration from environment variables
 DB_HOST = os.getenv('DATABASE_HOST', 'web-app-application-postgresql')
